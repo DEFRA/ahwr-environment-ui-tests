79f6c223d533dc2ca65a28d1c1593f9615d287e9
berlin-ee <berlin.bernadin@equalexperts.com>
2026-05-28 14:49:37 +0100

    feat(AHWR-1875) - Adding poultry journeys

--- test/utils/selectors.js
+++ test/utils/selectors.js
@@ -13,6 +13,7 @@ export const START_CLAIM_BUTTON = '#start'
 export const CONTINUE_BUTTON = 'button[id="continue"]'
 export const SUBMIT_CLAIM_BUTTON = '#submit-claim'
 export const ACCEPT_COOKIES_BUTTON = 'button[value="accept"]'
+export const SIGNOUT_BUTTON = 'a[href="/sign-out"]'
 
 // Checkboxes
 export const T_AND_C_CHECKBOX = '#terms'
@@ -22,7 +23,18 @@ export const BEEF_RADIO = 'input[value="beef"]'
 export const REVIEW_RADIO = 'input[value="review"]'
 export const YES_RADIO = 'input[value="yes"]'
 export const SAMPLES_TAKEN_SAME_DAY_RADIO = '#whenTestingWasCarriedOut'
-export const POSTIVE_TEST_RADIO = 'input[value="positive"]'
+export const POSITIVE_TEST_RADIO = 'input[value="positive"]'
+export const POULTRY_TYPE_DUCKS = 'input[name="typesOfPoultry"][value="ducks"]'
+export const MINIMUM_NUMBER_OF_BIRDS_SELECTOR_YES =
+  'input[name="minimumNumberOfBirds"][value="yes"]'
+export const BIOSECURITY_YES = 'input[name="biosecurity"][value="yes"]'
+export const BIOSECURITY_USEFULNESS_VERY_USEFUL =
+  'input[name="biosecurityUsefulness"][value="very-useful"]'
+export const CHANGES_IN_BIOSECURITY_CLEANING =
+  'input[name="changesInBiosecurity"][value="cleaning"]'
+export const COST_OF_CHANGES_NO_INTENTION =
+  'input[name="costOfChanges"][value="no-intention"]'
+export const INTERVIEW_YES = 'input[name="interview"][value="yes"]'
 
 // Text fields
 export const VISIT_DATE_DAY_FIELD = '#visit-date-day'
